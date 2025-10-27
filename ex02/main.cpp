63db52436d92aea113e86e07e8ff88553c869652
vboxuser <vboxuser@debian12.myguest.virtualbox.org>
2025-10-27 14:07:18 +0100

    ex02 done

---
+++ ex02/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include "MutantStack.hpp"
+#include <list>
+
+int main() {
+    MutantStack<int> mstack;
+
+    mstack.push(5);
+    mstack.push(17);
+
+    std::cout << "Top: " << mstack.top() << std::endl;
+
+    mstack.pop();
+    std::cout << "Size after pop: " << mstack.size() << std::endl;
+
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    mstack.push(0);
+
+    std::cout << "MutantStack contents:" << std::endl;
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+    ++it;
+    --it;
+    while (it != ite) {
+        std::cout << *it << std::endl;
+        ++it;
+    }
+
+    // Prueba de copy-constructor
+    std::stack<int> s(mstack);
+
+    std::cout << "\nComparativa con std::list:" << std::endl;
+    std::list<int> lst;
+    lst.push_back(5);
+    lst.push_back(17);
+    lst.pop_back();
+    lst.push_back(3);
+    lst.push_back(5);
+    lst.push_back(737);
+    lst.push_back(0);
+
+    for (std::list<int>::iterator lit = lst.begin(); lit != lst.end(); ++lit)
+        std::cout << *lit << std::endl;
+
+    return 0;
+}
